bbe705408c95e4fc1baa4d2309b20cb720df9b1e
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2011-07-19 22:10:00 -0400

    Added a password to the user and some encryption for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
+require 'digest'
+
+
+
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -10,4 +15,32 @@ class User < ActiveRecord::Base
                     :format => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
 
+  validates :password,  :presence => true,
+                        :confirmation => true,
+                        :length => { :within => 6..40 }
+
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  private
+
+  def encrypt_password
+    self.salt = make_salt if new_record?
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
 end
