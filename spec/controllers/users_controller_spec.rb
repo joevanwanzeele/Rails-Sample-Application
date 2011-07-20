a6edb84ccdac9eb79f7e7162f5930c9106161cda
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2011-07-20 19:48:41 -0400

    work on the view user and authentication

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,26 @@
 require 'spec_helper'
 
 
+
 describe UsersController do
   render_views
+
+  describe "GET 'show'" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+  end
 
   describe "GET 'new'" do
     it "should be successful" do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -104,7 +104,7 @@ describe User do
     end
 
     describe "has_password? method" do
-      
+
       it "should be true if the passwords match" do
         @user.has_password?(@attr[:password]).should be_true
       end
@@ -112,9 +112,28 @@ describe User do
       it "should be false if the passwords don't match" do
         @user.has_password?("invalid").should be_false
       end
+
+    end
+
+    describe "authentication method" do
+
+      it "should return nil on email/password mismatch" do
+        wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+        wrong_password_user.should be_nil
+      end
+
+      it "should return nil for an email address with no user" do
+        nonexistant_user = User.authenticate("bar@foo.com", @attr[:password])
+        nonexistant_user.should be_nil
+      end
+
+      it "should return the user on email/password match" do
+        matching_user = User.authenticate(@attr[:email], @attr[:password])
+        matching_user.should == @user
+      end
       
     end
-    
+
   end
-  
+
 end
