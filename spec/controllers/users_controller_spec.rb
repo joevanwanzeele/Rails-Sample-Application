776c9b781116a1b386896629d109b719879e0304
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2011-07-20 23:20:15 -0400

    User model with passwords and user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
-
-
 describe UsersController do
   render_views
 
@@ -19,7 +17,16 @@ describe UsersController do
       get :show, :id => @user
       response.should be_success
     end
-
+    
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+  
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
   end
 
   describe "GET 'new'" do
