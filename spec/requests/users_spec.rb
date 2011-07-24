31672a611117b1d3ff019332e6c955eb16a01f3b
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2011-07-24 18:48:18 -0400

    cleaned up users tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -42,7 +42,7 @@ describe "Users" do
     
     describe "failure" do
       it "should not a sign a user in" do
-        visit signin_path
+       visit signin_path
         fill_in :email,   :with => ""
         fill_in :password,:with => ""
         click_button
@@ -52,11 +52,7 @@ describe "Users" do
     
     describe "success" do
       it "should sign a user in and out" do
-        user = Factory(:user)
-        visit signin_path
-        fill_in :email,   :with => user.email
-        fill_in :password,:with => user.password
-        click_button
+       integration_sign_in(Factory(:user))
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
