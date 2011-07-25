f90ac894fb5f64c7eb0652264cbe5d648f0d2e0c
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2011-07-24 20:39:00 -0400

    work on updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @title = @user.name
@@ -21,5 +23,27 @@ class UsersController < ApplicationController
       @user.password_confirmation = ''
       render 'new'
     end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def authenticate
+    deny_access unless signed_in?
   end
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -73,5 +73,11 @@ describe "LayoutLinks" do
                                          :content => "Profile")
     end
     
+    it "should have a settings link" do
+      visit root_path
+      response.should have_selector("a", :href => edit_user_path(@user),
+                                         :content => "Settings")
+    end
+    
   end
 end
