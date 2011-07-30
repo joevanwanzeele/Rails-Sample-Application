ca9d051fe726c9bb99c73b2ee199eaa546079d43
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2011-07-30 12:16:31 -0400

    Added user microposts

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
   user.name                   "Joe VanWanzeele"
-  user.email                  "joe@buymyart.com"
+  user.email                  "bob@testbob.com"
   user.password               "foobar"
   user.password_confirmation  "foobar"
 end
