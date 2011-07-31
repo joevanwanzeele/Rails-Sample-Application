a84a05299dc72cea8a6df5904b64c55091b9dc4e
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2011-07-31 14:16:08 -0400

    work on the follower/following stuff

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,23 +1,46 @@
 require 'spec_helper'
 
+
+
 describe PagesController do
   render_views
 
   before(:each) do
-    @base_title = "Ruby on Rails Tutorial Sample App | "    
+    @base_title = "Ruby on Rails Tutorial Sample App | "
   end
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
+
+    describe "when not signed in" do
+
+      it "should be successful" do
+        get 'home'
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get 'home'
+        response.should have_selector("title",:content => @base_title + "Home")
+      end
+
     end
 
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",:content => @base_title + "Home")
-    end
+    describe "when signed in" do
 
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_selector("a", :href => following_user_path(@user),
+                                           :content => "0 following")
+        response.should have_selector("a", :href => followers_user_path(@user),
+                                           :content => "1 follower")
+      end
+    end
   end
 
   describe "GET 'contact'" do
@@ -43,15 +66,15 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",:content => @base_title + "About")
     end
-    
+
   end
-  
+
   describe "Get 'help'" do
     it "should be successful" do
       get 'help'
-      response.should be_success     
+      response.should be_success
     end
-    
+
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",:content => @base_title + "Help")
