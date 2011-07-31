c047d0eec9c94cb0942c546e6adf629befec1c5b
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2011-07-30 20:27:48 -0400

    added character count for the micropost input

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$( function() {
+	$('#micropost_content').keyup( function() {		
+		var remaining = 140 - $('#micropost_content').val().length;
+		$('#wordcount').html(Math.max(remaining, 0));
+		if (remaining <= 0)		
+			$('.wordcount').addClass('too_many_characters');
+		else		
+			$('.wordcount').removeClass('too_many_characters');
+	});
+});
